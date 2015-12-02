d08ae83a22b6231805c3e731a15d68380c3b5e12
Emilian Cyc <emiliancyc@gmail.com>
2015-12-02 20:58:24 +0100

    Added empty files designed for IP header

---
+++ src/ipheader.h
@@ -0,0 +1,17 @@
+/*
+ * ipheader.h
+ *
+ *  Created on: Dec 2, 2015
+ *      Author: emilian
+ */
+
+#ifndef IPHEADER_H_
+#define IPHEADER_H_
+
+class ip_header {
+public:
+	ip_header();
+	virtual ~ip_header();
+};
+
+#endif /* IPHEADER_H_ */
